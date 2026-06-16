1bb0df9fbcd9804455eea25008a46538bc4bd5be
ANNAMANENI HARSHA VARDHAN <aharshavardhan46@gmail.com>
2026-06-16 14:27:43 +0530

    Time: 1 ms (98.02%), Space: 42.8 MB (8.43%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        int l = 0, r = x, ans = 0;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            if ((long) m * m <= x) {
+                ans = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+
+        return ans;
+    }
+}
